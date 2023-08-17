0f0c3a91b21b985c678abdcd8254f0fbb8d159bd
Beko98 <99834191+Beko98@users.noreply.github.com>
2023-08-17 19:44:37 +0400

    searchbar suggestions

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,17 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { searchMovies } from "../components/Api";
 import { BiSearch } from "react-icons/bi";
+import "../scss/_SearchForm.scss";
 
 const SearchForm = ({ onSearch }) => {
   const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [suggestionClicked, setSuggestionClicked] = useState(false);
 
   const handleSearch = async () => {
     const movies = await searchMovies(query);
     onSearch(movies);
   };
 
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+      setSuggestionClicked(true);
+    }
+  };
+
+  const handleQueryChange = (newQuery) => {
+    setQuery(newQuery);
+    setSuggestionClicked(false);
+  };
+
+  const fetchMovies = useCallback(async () => {
+    try {
+      const movies = await searchMovies(query);
+      setSuggestions(movies);
+    } catch (error) {
+      console.error("Error fetching movie data:", error);
+    }
+  }, [query]);
+
+  useEffect(() => {
+    if (query) {
+      fetchMovies();
+    } else {
+      setSuggestions([]);
+    }
+  }, [query, fetchMovies]);
+
+  const handleSuggestionClick = (suggestion) => {
+    setQuery(suggestion.title);
+    handleSearch();
+    setSuggestionClicked(true);
+  };
+
   return (
-    <div>
+    <div className="search-container">
       <button className="searchBtn" onClick={handleSearch}>
         <BiSearch />
       </button>
@@ -20,8 +58,18 @@ const SearchForm = ({ onSearch }) => {
         type="text"
         placeholder="Search for a movie..."
         value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        onChange={(e) => handleQueryChange(e.target.value)}
+        onKeyPress={handleKeyPress}
       />
+      {suggestionClicked ? null : (
+        <ul className="suggestions">
+          {suggestions.map((suggestion, index) => (
+            <li key={index} onClick={() => handleSuggestionClick(suggestion)}>
+              {suggestion.title}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
